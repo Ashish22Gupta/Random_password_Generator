326d2baecd80af7eb7d0628e0754c9a1f077c95c
Gupta Ashish Maharanidin <154006215+Ashish22Gupta@users.noreply.github.com>
2025-01-10 18:30:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-var a = document.getElementById("otpg");
-    function run(){ 
-        var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        var pl = 4*3;
-        var a = "";
-        for (var i = 1; i <= pl; i++) {
+function run() { 
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var length = document.getElementById('length').value;
+    var password = "";
+    for (var i = 0; i < length; i++) {
         var randomNumber = Math.floor(Math.random() * chars.length);
-        a = a + chars.substring(randomNumber, randomNumber + 1);
-        }
-        document.getElementById("otpg").value = a;
-        c=a;
-        }
-
-    function copy() {
-      var ct = document.getElementById("otpg");
-      ct.select();
-      ct.setSelectionRange(0, 999);
-      document.execCommand("copy");
+        password += chars[randomNumber];
     }
+    document.getElementById("otpg").value = password;
+}
+
+function copy() {
+    var ct = document.getElementById("otpg");
+    ct.select();
+    ct.setSelectionRange(0, 999);
+    document.execCommand("copy");
+    alert("Password copied successfully!");
+}
+
+function updateLength(value) {
+    document.getElementById("lengthValue").textContent = value;
+}
